2cef3abb9175f6060b7c8b044c64cb5f6adf8417
Ryo Shen <5581972+lwshen@users.noreply.github.com>
2025-05-14 00:48:43 +0800

    feat: support light mode and update icon (#9)

--- web/src/components/theme-mode-toggle.tsx
+++ web/src/components/theme-mode-toggle.tsx
@@ -9,7 +9,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { useTheme } from '@/hooks/use-theme';
 
-export function ModeToggle() {
+export function ThemeToggle() {
   const { setTheme } = useTheme();
 
   return (
