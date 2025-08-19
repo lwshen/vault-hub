4b3e6f596eefc38c47c7c7a5639780acadf0f90b
Ryo Shen <5581972+lwshen@users.noreply.github.com>
2025-08-19 17:37:07 +0800

    fix: api key authentication in header and fix yaml linting issues (#160)

--- scripts/format-yaml.sh
+++ scripts/format-yaml.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # For all YAML files
-find .github/workflows -maxdepth 1 \( -name "*.yml" -o -name "*.yaml" \) -type f | while read -r file; do
+find . -maxdepth 5 \( -name "*.yml" -o -name "*.yaml" \) -type f ! -path "./apps/web/*" ! -path "./packages/api/api.bundled.yaml" | while read -r file; do
     echo "Formatting $file"
     yq -i -P "$file"
 done
