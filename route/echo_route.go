be1ecb2a88b5b86eaa99cae676b61316a5e56aad
Ryo Shen <shenlw66@gmail.com>
2025-10-27 22:59:56 +0800

    fix spa frontend

--- route/echo_route.go
+++ route/echo_route.go
@@ -1,10 +1,12 @@
 package route
 
 import (
+	"io/fs"
 	"net/http"
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/lwshen/vault-hub/internal/embed"
 )
 
 // SetupEchoRoutes configures Echo router with all routes and middleware
@@ -65,7 +67,38 @@ func SetupEchoRoutes(e *echo.Echo) error {
 
 	// TODO: Add remaining endpoints with proper handlers
 	// Vault endpoints, API key endpoints, etc.
-	// TODO: Add static file serving when needed
+
+	// Setup static file serving from embedded filesystem
+	distFS, err := embed.GetDistFS()
+	if err != nil {
+		return err
+	}
+
+	// Serve static assets (CSS, JS, images, etc.)
+	e.GET("/assets/*", func(c echo.Context) error {
+		return echo.WrapHandler(http.FileServer(http.FS(distFS)))(c)
+	})
+
+	// Serve other static files (fonts, icons, etc.)
+	e.GET("/fonts/*", func(c echo.Context) error {
+		return echo.WrapHandler(http.FileServer(http.FS(distFS)))(c)
+	})
+	e.GET("/icon.svg", func(c echo.Context) error {
+		return echo.WrapHandler(http.FileServer(http.FS(distFS)))(c)
+	})
+	e.GET("/vite.svg", func(c echo.Context) error {
+		return echo.WrapHandler(http.FileServer(http.FS(distFS)))(c)
+	})
+
+	// Serve index.html for all other routes (SPA routing)
+	// This should be added last to act as a catch-all for client-side routes
+	e.GET("/*", func(c echo.Context) error {
+		indexHTML, err := fs.ReadFile(distFS, "index.html")
+		if err != nil {
+			return c.JSON(http.StatusNotFound, map[string]string{"error": "Page not found"})
+		}
+		return c.HTMLBlob(http.StatusOK, indexHTML)
+	})
 
 	return nil
 }
